b68979e31aefa00bff7c60cd7fb19d3044934a4c
Oleg Tsyganov <redsmoke19@gmail.com>
2023-08-16 00:22:13 +0400

    Enum

--- basic.ts
+++ basic.ts
@@ -50,3 +50,42 @@ const skillTuples: [number, string] = [1, "HTML"];
 const [id, skillName] = skillTuples;
 
 const skillTuples2: [number, string, ...boolean[]] = [1, "HTML", true, false];
+
+// Readonly
+const skillsReadonly: ReadonlyArray<string> = ["HTML", "CSS", "JS"];
+const skillsReadonly2: readonly  string[] = ["HTML", "CSS", "JS"];
+
+// Enum
+// Это что то вроде справочника, который имеет ограниченное число значений
+enum StatusCode {
+  SUCCESS = 1,
+  PROCESSING = 2,
+  ERROR = 3
+}
+
+const example1 = {
+  message: "Успешно",
+  status: StatusCode.SUCCESS
+}
+
+if (example1.status === StatusCode.SUCCESS) {
+  console.log(example1.message);
+}
+
+// Это гетерагенный enum
+enum StatusCode2 {
+  SUCCESS = "o",
+  PROCESSING = 2,
+  ERROR = 3
+}
+
+function enumAction(status: StatusCode2) {}
+enumAction(2); // Тут окей
+enumAction(StatusCode2.SUCCESS); // Тут окей
+// enumAction("o"); Тут ошибка, потому что хоть он гетерагенный, по умолчанию это цифровой enum
+
+// Константные Enum не компилируются в функцию и ее можно использовать как обычный map в вычислениях или еще где то 
+const enum Roles {
+  ADMIN = 2,
+  USER = 1,
+}
