d4e835575af7a827e398bb072786c3a026d6cf13
Oleg Tsyganov <redsmoke19@gmail.com>
2023-08-15 23:01:19 +0400

    Упражнение - Типизируем объект

---
+++ test.ts
@@ -0,0 +1,33 @@
+const office = {
+  "officeId": 45,
+  "isOpened": false,
+  "contacts": {
+    "phone": "050-555-55",
+    "email": "XXXXXXXXXXXXXXXX",
+    "address": {
+      "city": "Minsk",
+    }
+  }
+}
+
+const info: {
+  officeId: number,
+  isOpened: boolean,
+  contacts: {
+    phone: string,
+    email: string,
+    address: {
+      city: string,
+    }
+  }
+} = {
+  "officeId": 45,
+  "isOpened": false,
+  "contacts": {
+    "phone": "050-555-55",
+    "email": "XXXXXXXXXXXXXXXX",
+    "address": {
+      "city": "Minsk",
+    }
+  }
+}
