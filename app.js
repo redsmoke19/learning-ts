7ccdb831935e9bb3afe2fd10d55cf0d4314b87e1
Oleg Tsyganov <redsmoke19@gmail.com>
2023-08-15 22:53:44 +0400

    объекты

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 "use strict";
-const a = 1;
+function getFullName(firstName, basename) {
+    return `${firstName} ${basename}`;
+}
+const getFullNameArr = (firstName, basename) => {
+    return `${firstName} ${basename}`;
+};
+const rev = 1000;
+const bonus = 500;
+const str = "hello";
+const bool = true;
+const res = rev + bonus;
+const getMyCar = (carEntity) => {
+    return `My car is ${carEntity.brand} ${carEntity.model} ${carEntity.year}`;
+};
+const car = {
+    brand: "Kia",
+    model: "Rio",
+    year: "2019",
+    wills: 2
+};
+console.log(getMyCar(car));
--- app.ts
+++ app.ts
@@ -4,7 +4,6 @@ function getFullName(firstName: string, basename: string): string {
 
 const getFullNameArr = (firstName: string, basename: string): string => {
   return `${firstName} ${basename}`;
-
 }
 
 const rev: number = 1000;
@@ -13,4 +12,16 @@ const str: string = "hello";
 const bool: boolean = true;
 
 const res: number = rev + bonus;
-console.log(res);
+
+const getMyCar = (carEntity: { brand: string, model: string, year: string }): string => {
+  return `My car is ${carEntity.brand} ${carEntity.model} ${carEntity.year}`;
+}
+
+const car = {
+  brand: "Kia",
+  model: "Rio",
+  year: "2019",
+  wills: 2
+}
+
+console.log(getMyCar(car));
